1e7f36b48fe339a99b0c7276889e3c1a03dec899
salam <chaudharysm00@gmail.com>
2025-12-24 20:35:06 +0545

    backend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'furniture-ri8t.vercel.app',
     credentials: true,
   })
 )
