05a89c8c94ece90eee126cea1b19503f635ae1b6
salam <chaudharysm00@gmail.com>
2025-12-24 12:18:01 +0545

    name changed

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authApi } from '../features/auth/authApi.js'
 import authReducer from '../features/auth/authSlice.js'
-import { productApi } from '../features/product/ProductApi.js'
-import { bannerImgApi } from '../features/bannerImg/BannerImgApi.js'
-import { blogsApi } from '../features/blogs/BlogsApi.js'
+import { productApi } from '../features/product/productApi.js'
+import { bannerImgApi } from '../features/bannerImg/bannerImgApi.js'
+import { blogsApi } from '../features/blogs/blogsApi.js'
 import cartReducer from '../features/cart/cartSlice.js'
 
 export const store = configureStore({
