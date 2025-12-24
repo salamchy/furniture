70b51c89e814e4f14ac50f11bccce82a68bfda7e
salam <chaudharysm00@gmail.com>
2025-12-24 21:17:52 +0545

    partationed

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -82,6 +82,7 @@ export const loginUser = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // true in production
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // 'none' for cross-site
+      partitioned: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       maxAge: 60 * 60 * 1000, //1hour
     })
